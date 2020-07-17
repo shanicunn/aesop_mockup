bb4127b292281e3938352a9801302a5344eae67a
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-17 11:21:03 -0400

    Architecture component created

---
+++ aesop-mockup/src/components/ArchComp.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// ARCHITECTURE AND DESIGN DIV
+class ArchComp extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+export default ArchComp;
