72421c66c808526424fd2265ada8753f37aae46d
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-17 12:02:29 -0400

    Arch component added to App.js

--- aesop-mockup/src/App.js
+++ aesop-mockup/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ArchComp from './components/ArchComp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    
+    {/* Architecture and Design */}
+      <ArchComp
+        
+      />
     </div>
   );
 }
