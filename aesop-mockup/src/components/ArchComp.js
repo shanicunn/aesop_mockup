2b36b9cebd8b3a6b05f178dc0b312a58133fe17e
Christopher Potti <christopherpotti@PSADMINs-MacBook-Air.local>
2020-07-17 14:52:12 -0400

    i replaced shanis code before she killed me

--- aesop-mockup/src/components/ArchComp.js
+++ aesop-mockup/src/components/ArchComp.js
@@ -1,16 +1,52 @@
 import React from 'react';
-// ARCHITECTURE AND DESIGN DIV
-class ArchComp extends React.Component {
-    constructor() {
-        super();
-        this.state = {
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+import
 
+ARCHITECTURE AND DESIGN DIV
+https://www.npmjs.com/package/react-slideshow-image
+const slideImages = [
+    '/aesop-mockup/src/assets/images/Aesop-Bleecker-Street-Carousel-1-Desktop-2560x1440px.jpg',
+    
+    '/aesop-mockup/src/assets/images/Aesop-Free-Market-Carousel-1-Desktop-2560x1440px.png',
+]
+ const Slideshow = () => {
+     return (
+         <div className="slide-container">
+             <Slide>
+                 <div className="each-slide">
+                     <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+                         <span>Slide 1</span>
+                     </div>
+                 </div>
+                 <div className="each-slide">
+                     <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+                         <span>Slide 2</span>
+                     </div>
+                 </div>
+             </Slide>
+         </div>
+     )
+ }
+class ArchComp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            header: "",
+            theme: "",
+            description: "",
+            function: "",
+            img: "",
         }
     }
     render() {
         return (
-            <div>
-
+            <div className="arch-comp">
+                <h3>Architecture and Design</h3>
+                <h2>Spaces of distinction</h2>
+                <h3>Aesop stores are informed by the history, fabric, and context of their local environments, yet retain an aesthetic consistency</h3>
+                <button> Find A Nearby Store </button>
+                <Slideshow />
             </div>
         )
     }
