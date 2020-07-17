50750066f2656f8f1e5e2ba21e296ff51cd2377f
Christopher Potti <christopherpotti@PSADMINs-MacBook-Air.local>
2020-07-17 14:06:30 -0400

    connected footer component to app.js

--- aesop-mockup/src/App.js
+++ aesop-mockup/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import ArchComp from './components/ArchComp';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
 
-    
+    <Footer/>
     {/* Architecture and Design */}
       <ArchComp
         
