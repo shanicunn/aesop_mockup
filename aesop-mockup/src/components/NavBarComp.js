2fcaddab4841f537e1951e198a3701554b6216ac
Student_GA126 <student_ga126@psxga126.lan>
2020-07-19 12:16:42 -0400

    Basic styling

--- aesop-mockup/src/components/NavBarComp.js
+++ aesop-mockup/src/components/NavBarComp.js
@@ -1,26 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class NavBarComp extends Component {
     render() {
+
         return (
             <div>
-                <ul className='navBar'>
+                <ul className='navBar' style={styles.navBar}> {/* Note syntax for styling inside of component. */}
                     <a href='#'>
-                        <li>Shop</li>
+                        <li style={styles.li}>Shop</li>
                     </a>
                     <a href='#'>
-                        <li>Read</li>
+                        <li style={styles.li}>Read</li>
                     </a>
                     <a href='#'>
-                        <li>Stores</li>
+                        <li style={styles.li}>Stores</li>
                     </a>
                     <a href='#'>
-                        <li>Search</li>
+                        <li style={styles.li}>Search</li>
+                    </a>
+                    <a href='#'>
+                        <li style={styles.li}>Log-in</li>
                     </a>
                 </ul>
+
 
                 
             </div>
         )
+    }
+}
+
+// Styling inside the component to avoid merge issues on css file
+const styles = {
+    navBar: {
+    listStyle: 'none',
+    display: 'flex',
+    // justifyContent: 'space-between'
+    },
+    li: {
+        paddingLeft: '10px'
+    },
+    login: {
+        position: 'aboslute',
     }
 }
