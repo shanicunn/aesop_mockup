685fa48fc1a86edf03e8cdf8f9ffcd55e200a2da
Christopher Potti <christopherpotti@PSADMINs-MacBook-Air.local>
2020-07-20 15:20:40 -0400

    the content div is complete the image will not show

--- aesop-mockup/src/components/Discovermore.js
+++ aesop-mockup/src/components/Discovermore.js
@@ -8,40 +8,42 @@ class Discovermore extends React.Component {
         render() { 
             return (
                 <div>
-                <img src='##' alt='product image'></img>
-                <h1>Discover More</h1>
+                    <img src='##' alt='product image'></img>
+                    <h1>Discover More</h1>
 
-                <h3>Kits & duets</h3>
-                <ul>
-                    <li>
-                        <a src='##'>Travel</a>
-                    </li>
-                    <li>
-                        <a src='##'>Gift Kits</a>
-                    </li>
-                    <li>
-                        <a src='##'>Skin</a>
-                    </li>
-                    <li>
-                        <a src='##'>Hand & body</a>
-                    </li>
-                </ul>
+                    <h3>Kits & duets</h3>
+                    <ul>
+                        <li>
+                            <a src='##'>Travel</a>
+                        </li>
+                        <li>
+                            <a src='##'>Gift Kits</a>
+                        </li>
+                        <li>
+                            <a src='##'>Skin</a>
+                        </li>
+                        <li>
+                            <a src='##'>Hand & body</a>
+                        </li>
+                    </ul>
 
-                <h3>Hair care</h3>
-                <ul>
-                    <li>
-                        <a src='##'>Cleanse</a>
-                    </li>
-                    <li>
-                        <a src='##'>Condition</a>
-                    </li>
-                    <li>
-                        <a src='##'>Treat</a>
-                    </li>
-                    <li>
-                        <a src='##'>Groom</a>
-                    </li>
-                </ul>
+                    <h3>Hair care</h3>
+                    <ul>
+                        <li>
+                            <a src='##'>Cleanse</a>
+                        </li>
+                        <li>
+                            <a src='##'>Condition</a>
+                        </li>
+                        <li>
+                            <a src='##'>Treat</a>
+                        </li>
+                        <li>
+                            <a src='##'>Groom</a>
+                        </li>
+                    </ul>
+                    <h1>'And your very flesh shall be a great poem'</h1>
+                    <h3>Walt Whitman</h3>
                 </div>
               );
         }
