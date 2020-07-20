d43f42b03f410b18ff62c693ad7e60f8703eb027
Student_GA126 <student_ga126@psxga126.lan>
2020-07-20 17:12:18 -0400

    Created essentials component branch

--- aesop-mockup/src/App.js
+++ aesop-mockup/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import ArchComp from './components/ArchComp';
-import NavBarComp from './components/NavBarComp';
+// import NavBarComp from './components/NavBarComp';
 
 function App() {
   return (
@@ -9,10 +9,10 @@ function App() {
 
     
     {/* Architecture and Design */}
-      {/* <ArchComp
+      <ArchComp
         
-      /> */}
-      <NavBarComp />
+      />
+      {/* <NavBarComp /> */}
     </div>
   );
 }
