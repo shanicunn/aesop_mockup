9482bc4eb8a1c689f7cbbcaeb491a0425a4d543e
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-07-20 22:30:10 -0400

    Created more divs for grid styling

--- aesop-mockup/src/components/Discovermore.js
+++ aesop-mockup/src/components/Discovermore.js
@@ -8,10 +8,11 @@ class Discovermore extends React.Component {
         render() { 
             return (
                 <div className="child discover">
+                <div className="image">
                     <img src='/src/assets/images/staple_products/Aesop-Hand-Resurrection-Aromatique-Hand-Wash-500mL-large.png' alt='product image'></img>
-
-                   
-                    <h1>Discover More</h1>
+                </div>
+                <div className="column1">
+                <h1>Discover More</h1>
 
                     <h3>Kits & duets</h3>
                     <ul>
@@ -28,8 +29,9 @@ class Discovermore extends React.Component {
                             <a src='##'>Hand & body</a>
                         </li>
                     </ul>
-
-                    <h3>Hair care</h3>
+                </div>
+                <div className="column2">
+                <h3>Hair care</h3>
                     <ul>
                         <li>
                             <a src='##'>Cleanse</a>
@@ -44,8 +46,16 @@ class Discovermore extends React.Component {
                             <a src='##'>Groom</a>
                         </li>
                     </ul>
+                </div>
+                <div className="row">
+   
                     <h1>'And your very flesh shall be a great poem'</h1>
                     <h3>Walt Whitman</h3>
+                </div>
+                   
+                    
+
+                 
                 </div>
               );
         }
