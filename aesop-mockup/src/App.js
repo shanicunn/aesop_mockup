6aaa957a3ea45f933100a0370e16ceff3e1765ce
Student_GA126 <student_ga126@psxga126.lan>
2020-07-22 03:16:06 -0400

    Added images and styling

--- aesop-mockup/src/App.js
+++ aesop-mockup/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ArchComp from './components/ArchComp';
+// import ArchComp from './components/ArchComp';
 import EssentialsComp from './components/EssentialsComp';
 
 function App() {
--- aesop-mockup/src/components/EssentialsComp.js
+++ aesop-mockup/src/components/EssentialsComp.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
+import image1 from '../assets/images/staple_products/Aesop-Hand-Resurrection-Aromatique-Hand-Wash-500mL-large.png';
+import image2 from '../assets/images/staple_products/Aesop-Body-Resurrection-Aromatique-Hand-Balm-75mL-large.png';
+import image3 from '../assets/images/staple_products/Large-PNG-Aesop-Personal-Deodorant-50mL-large.png';
+import image4 from '../assets/images/staple_products/Aesop-Skin-Purifying-Facial-Exfoliant-Paste-75mL-large.png';
 
 export default class EssentialsComp extends Component {
     render() {
         return (
             <div>
                 <section className='child essentials'>
-                    {/* Left side of component: Titles and description */}
+                    <div className='description'>
+                        {/* Left side of component: Titles and description */}
+                        {/* Titles */}
+                        <h5>For the body</h5>
+                        <h2>An expression of care</h2>
 
-                    {/* Titles */}
-                    <h5>Resurrction Portable Hand Care</h5>
-                    <h2>Pocket-sized essentials</h2>
-                    {/* Description text */}
-                    <p>A refreshing evaporative gel and richly nourishing balm; conveniently paired for use at home, at work or out and about.</p> 
-                    {/* Button under paragraph */}
-                    <span> Discover the duo -> </span> 
+                        {/* Description text */}
+                        <p>Aesop formulations offer the body deserving care, to cleanse, replenish, and nourish skin. Each product is a sensory pleasure to use with its own delightful aroma</p> 
+                    </div>
 
-                    {/* Right side of component: Image  */}
+                    <div className='product-imgs'>
+                        {/* Right side of component: Images */}
+                        <img src={image1} alt='' height='300px' />
+
+                        <img src={image2} alt='' height='300px' />
+
+                        <img src={image3} alt='' height='300px' />
+
+                        <img src={image4} alt='' height='300px' />
+
+                    </div>
 
                 </section>
             </div>
